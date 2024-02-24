a8916103b76d7a21d6782ea1bf16e17b1d5c0351
Matheus Lucas <m7hsouza@gmail.com>
2024-02-24 10:15:57 -0400

    feat: persistir os dados em um arquivo json

--- src/http/database/connection.js
+++ src/http/database/connection.js
@@ -1,9 +1,26 @@
+import fs from 'node:fs/promises'
+
+const databasePath = new URL('../../../database.json', import.meta.url)
+
 class Connection {
+  constructor () {
+    fs.readFile(databasePath)
+      .then(data => {
+        this.#tables = JSON.parse(data)
+      })
+      .catch(() => this.#persist())
+  }
+  
   #tables = {}
 
-  insert (table, data) {
+  async #persist () {
+    await fs.writeFile(databasePath, JSON.stringify(this.#tables));
+  }
+
+  async insert (table, data) {
     this.#tables[table] ??= []
     this.#tables[table].push(data)
+    await this.#persist();
   }
 
   select (table) {
